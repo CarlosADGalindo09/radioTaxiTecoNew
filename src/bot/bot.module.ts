d58ec0cf7ad68bb63d116d11d56641cbd17fccd6
CarlosGalindo09 <carlosgalindosdt@gmail.com>
2025-08-25 23:33:10 -0600

    Actualizacion HttpModule

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
 import { BotController } from './bot.controller';
-import { HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports:[HttpService],
+  imports:[HttpModule],
   controllers: [BotController],
   providers: [BotService],
 })
